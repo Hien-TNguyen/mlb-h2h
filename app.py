4f8544ce7efee48dd98271bc3cd81eacf0531eec
Nguyen Hien <hiennguyen83@gmail.com>
2018-08-15 14:14:13 -0700

    make changes in app.py and record file

--- app.py
+++ app.py
@@ -40,16 +40,31 @@ def show_teams():
 @app.route("/team/<string:franchID>/")
 def display_record(franchID):
     cur, conn = connection()
-    query = """SELECT name, yearID, W , L, COALESCE(ROUND(W/(W+L),3)) as percentWin
-                FROM Teams where franchID= '{}';""".format(franchID)
+    query = """select distinct T.name, T.teamID, T.franchID, T.W, T.L,
+            COALESCE(ROUND(T.W/(T.W+T.L),3)) as percentWin from Teams T JOIN
+            TeamsFranchises TF ON T.franchID = TF.franchID Where TF.active="Y"
+            AND T.name=TF.franchName AND T.franchID ='{}';""".format(franchID)
     cur.execute(query)
     records = cur.fetchall()
     conn.close()
     return render_template("record.html", record=records)
 
-@app.route("/SFG-LAD")
-def displayComparison():
-    return render_template("compare.html")
+@app.route("/<string:teamID1>-<string:teamID2>/")
+def displayComparison(teamID1, teamID2):
+    cur, conn = connection()
+    query = """SELECT any_value(home), any_value(visitor), max(YEAR(date)),
+                SUM(case when ((home_score>visitor_score and home='{0}') OR
+                (home_score<visitor_score and home='{1}')) then 1 else 0 end) as Team1_WIN,
+                SUM(case when ((home_score<visitor_score and home='{0}') OR
+                (home_score>visitor_score and home='{1}')) then 1 else 0 end) as Team2_WIN
+                from games
+                where (visitor="SFN" OR visitor="LAN") and (home="LAN" OR home="SFN")
+                GROUP BY YEAR(date);""".format(teamID1, teamID2)
+    cur.execute(query)
+    records = cur.fetchall()
+    conn.close()
+
+    return render_template("h2h.html", )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
